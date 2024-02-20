c9f0feec57d9a23d4bbdf19a77c6848228a47c72
FrinshHD <frinshhd@gmail.com>
2024-02-20 19:42:41 +0100

    added hex color support

--- src/main/java/de/frinshhd/anturniaquests/utils/MessageFormat.java
+++ src/main/java/de/frinshhd/anturniaquests/utils/MessageFormat.java
@@ -2,10 +2,28 @@ package de.frinshhd.anturniaquests.utils;
 
 import org.bukkit.ChatColor;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MessageFormat {
 
-    public static String build(String input) {
-        return ChatColor.translateAlternateColorCodes('&', input);
+    public static String build(String message) {
+        Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
+        Matcher matcher = pattern.matcher(message);
+        while (matcher.find()) {
+            String hexCode = message.substring(matcher.start(), matcher.end());
+            String replaceSharp = hexCode.replace('#', 'x');
+
+            char[] ch = replaceSharp.toCharArray();
+            StringBuilder builder = new StringBuilder("");
+            for (char c : ch) {
+                builder.append("&" + c);
+            }
+
+            message = message.replace(hexCode, builder.toString());
+            matcher = pattern.matcher(message);
+        }
+        return ChatColor.translateAlternateColorCodes('&', message);
     }
 
 }
