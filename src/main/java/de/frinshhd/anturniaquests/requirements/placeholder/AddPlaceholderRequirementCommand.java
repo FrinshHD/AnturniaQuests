6bf07f283092b4262ae9e05a40b3e7ac9f7b728a
FrinshHD <frinshhd@gmail.com>
2025-06-24 12:16:09 +0200

    added AddPlaceholderRequirementCommand

--- src/main/java/de/frinshhd/anturniaquests/requirements/placeholder/AddPlaceholderRequirementCommand.java
+++ src/main/java/de/frinshhd/anturniaquests/requirements/placeholder/AddPlaceholderRequirementCommand.java
@@ -15,8 +15,20 @@ import java.util.List;
 public class AddPlaceholderRequirementCommand extends BasicSubCommand {
 
     public AddPlaceholderRequirementCommand() {
-        super("quests", "anturniaquests.command.admin.quests.add.requirement", new String[]{"edit", "<questID>", "add", "requirement", "permission", "<permission>"});
-        setDescription("Adds a permission requirement to a quest.");
+        super("quests", "anturniaquests.command.admin.quests.add.requirement",
+                new String[]{
+                        "edit",
+                        "<questID>",
+                        "add",
+                        "requirement",
+                        "placeholder",
+                        "<placeholder>",
+                        "<comparisonType>",
+                        "<comparisonOperator>",
+                        "<comparison>",
+                        "[name]"
+                });
+        setDescription("Adds a placeholder requirement to a quest.");
     }
 
     @Override
@@ -25,8 +37,8 @@ public class AddPlaceholderRequirementCommand extends BasicSubCommand {
             return false;
         }
 
-        if (args.length < 6) {
-            Main.getCommandManager().getSubCommand(Main.getCommandManager().getCommand(getMainCommand()), "help").execute(sender, commandLabel, new String[]{"edit", "<questID>", "add", "requirement", "permission"});
+        if (args.length < 9) {
+            Main.getCommandManager().getSubCommand(Main.getCommandManager().getCommand(getMainCommand()), "help").execute(sender, commandLabel, new String[]{"edit", "<questID>", "add", "requirement", "placeholder"});
             return true;
         }
 
@@ -37,19 +49,50 @@ public class AddPlaceholderRequirementCommand extends BasicSubCommand {
             return true;
         }
 
+        String placeholder = args[5];
+        String comparisonTypeRaw = args[6];
+        String comparisonOperatorRaw = args[7];
+        String comparison = args[8];
 
-        String permission = args[5];
+        String name = null;
+        if (args.length >= 10) {
+            name = args[9];
+        }
+
+        ComparisonType comparisonType = null;
+
+        try {
+            comparisonType = ComparisonType.valueOf(comparisonTypeRaw);
+        } catch (IllegalArgumentException e) {
+            ChatManager.sendMessage(sender, "Illegal comparison type: " + comparisonTypeRaw);
+            return true;
+        }
+
+        ComparionOperator comparisonOperator = null;
+
+        try {
+            comparisonOperator = ComparionOperator.valueOf(comparisonOperatorRaw);
+        } catch (IllegalArgumentException e) {
+            ChatManager.sendMessage(sender, "Illegal comparison operator: " + comparisonOperatorRaw);
+            return true;
+        }
 
         Quest quest = Main.getQuestsManager().getEditableQuest(questID);
 
         PlaceholderModel placeholderModel = new PlaceholderModel(new LinkedHashMap<>());
-        //placeholderModel.setPermission(permission);
+        placeholderModel.setPlaceholder(placeholder);
+        placeholderModel.setComparisonType(comparisonType);
+        placeholderModel.setComparisonOperator(comparisonOperator);
+        placeholderModel.setComparison(comparison);
+        if (name != null) {
+            placeholderModel.setName(name);
+        }
 
-        quest.addRequirement("permissions", placeholderModel);
+        quest.addRequirement("placeholders", placeholderModel);
 
         Main.getQuestsManager().saveQuestToYml(questID, quest);
 
-        ChatManager.sendMessage(sender, TranslationManager.getInstance().build("quests.command.edit.add.requirement.permission", new Translatable("questID", questID), new Translatable("permission", permission)));
+        ChatManager.sendMessage(sender, TranslationManager.getInstance().build("quests.command.edit.add.requirement.placeholder", new Translatable("questID", questID), new Translatable("name", placeholderModel.getName())));
         return true;
     }
 
@@ -61,7 +104,7 @@ public class AddPlaceholderRequirementCommand extends BasicSubCommand {
         List<String> possibleCompletions = new ArrayList<>();
         List<String> completions = new ArrayList<>();
 
-        possibleCompletions.add("permission");
+        possibleCompletions.add("placeholder");
 
         // Filter
         possibleCompletions.forEach(completion -> {
@@ -69,6 +112,24 @@ public class AddPlaceholderRequirementCommand extends BasicSubCommand {
                 completions.add(completion);
             }
         });
+
+        if (args.length == 7) {
+            for (ComparisonType comparisonType : ComparisonType.values()) {
+                String comparisonTypeName = comparisonType.name();
+                if (comparisonTypeName.toLowerCase().startsWith(args[5].toLowerCase())) {
+                    completions.add(comparisonTypeName.toLowerCase());
+                }
+            }
+        }
+
+        if (args.length == 8) {
+            for (ComparionOperator comparisonOperator : ComparionOperator.values()) {
+                String comparisonOperatorName = comparisonOperator.name();
+                if (comparisonOperatorName.toLowerCase().startsWith(args[6].toLowerCase())) {
+                    completions.add(comparisonOperatorName.toLowerCase());
+                }
+            }
+        }
 
         return completions;
     }
